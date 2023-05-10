6cd14d83247e136e27d416c5d3232d0acc62f8c6
Aries Wibowo <riespratama@gmail.com>
2023-05-10 13:52:17 +0700

    Redirect empty url to id 1

--- wbDisplay/urls.py
+++ wbDisplay/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.displayBoard.as_view(), name='displayBoard'),
+    path('', views.indexView.as_view(), name='displayBoard'),
     path('<int:pk>', views.displayBoard.as_view(), name='displayBoard'),
     path('update/<int:pk>', views.gantiNama.as_view(), name='update'),
 ]
--- wbDisplay/views.py
+++ wbDisplay/views.py
@@ -1,5 +1,10 @@
-from django.views.generic import TemplateView, UpdateView, DetailView
+from django.views.generic import TemplateView, UpdateView, DetailView, RedirectView
 from .models import namaDisplay
+from django.conf import settings
+from pathlib import os
+
+class indexView(RedirectView):
+    url = '/1'#os.path.join(settings.BASE_DIR, '1')
 
 class displayBoard(DetailView):
     template_name = 'index.html'
