f77c86238c7c3dfae3bea822bbaf9300bc762266
Aries Wibowo <riespratama@gmail.com>
2024-12-10 11:21:03 +0700

    revisi susunan dan database

--- wbDisplay/models.py
+++ wbDisplay/models.py
@@ -2,9 +2,9 @@ from django.db import models
 from django.urls import reverse
 
 class namaDisplay(models.Model):
-    titlevip = models.CharField(max_length=128)
-    namavip = models.CharField(max_length=64)
-    perusahaan = models.CharField(max_length=128)
+    titlevip = models.CharField(max_length=128, null=True, blank=True)
+    namavip = models.CharField(max_length=64, null=True, blank=True)
+    perusahaan = models.CharField(max_length=128, null=True, blank=True)
     jam = models.TimeField()
 
     def get_absolute_url(self):
